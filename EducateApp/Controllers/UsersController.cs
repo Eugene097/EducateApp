079ef4340f80a783f40e0ad98ae613824ddd4089
Евгений Куканов <3gt1@mail.ru>
2021-10-29 12:10:23 +0500

    Добавление функции управления ролями, добавление RoleInitializator

--- EducateApp/Controllers/UsersController.cs
+++ EducateApp/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using EducateApp.ViewModels.Users;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,19 +11,19 @@ namespace EducateApp.Controllers
     public class UsersController : Controller
     {
         UserManager<User> _userManager;
+        RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(UserManager<User> userManager)
+        public UsersController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+        // отображение списка пользователей
         // действия для начальной страницы Index
         public IActionResult Index() => View(_userManager.Users.ToList());
-
-
         // действия для создания пользователя Create
         public IActionResult Create() => View();
-
         [HttpPost]
         public async Task<IActionResult> Create(CreateUserViewModel model)
         {
@@ -51,8 +52,6 @@ namespace EducateApp.Controllers
             }
             return View(model);
         }
-
-
         // действия для изменения пользователя Edit
         public async Task<IActionResult> Edit(string id)
         {
@@ -71,7 +70,6 @@ namespace EducateApp.Controllers
             };
             return View(model);
         }
-
         [HttpPost]
         public async Task<IActionResult> Edit(EditUserViewModel model)
         {
@@ -85,7 +83,6 @@ namespace EducateApp.Controllers
                     user.LastName = model.LastName;
                     user.Name = model.Name;
                     user.Patronymic = model.Patronymic;
-
                     var result = await _userManager.UpdateAsync(user);
                     if (result.Succeeded)
                     {
@@ -102,16 +99,14 @@ namespace EducateApp.Controllers
             }
             return View(model);
         }
-
-
         // действия для удаления пользователя Delete с подтверждением
+        // GET: Users/Delete/5
         public async Task<ActionResult> Delete(string id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             User user = await _userManager.FindByIdAsync(id);
             if (user == null)
             {
@@ -119,7 +114,7 @@ namespace EducateApp.Controllers
             }
             return View(user);
         }
-
+        // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
@@ -128,7 +123,6 @@ namespace EducateApp.Controllers
             IdentityResult result = await _userManager.DeleteAsync(user);
             return RedirectToAction(nameof(Index));
         }
-
         public async Task<IActionResult> ChangePassword(string id)
         {
             User user = await _userManager.FindByIdAsync(id);
@@ -136,11 +130,13 @@ namespace EducateApp.Controllers
             {
                 return NotFound();
             }
-            ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, Email = user.Email };
+            ChangePasswordViewModel model = new ChangePasswordViewModel
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
             return View(model);
         }
-
-        //Смена пароля
         [HttpPost]
         public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
         {
@@ -153,7 +149,6 @@ namespace EducateApp.Controllers
                         HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
                     var _passwordHasher =
                         HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
-
                     IdentityResult result =
                         await _passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
                     if (result.Succeeded)
@@ -176,6 +171,54 @@ namespace EducateApp.Controllers
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> EditRoles(string userId)
+        {
+            // получаем пользователя
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRoles(string userId, List<string> roles)
+        {
+            // получаем пользователя
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // получем список ролей пользователя
+                var userRoles = await _userManager.GetRolesAsync(user);
+                // получаем все роли
+                var allRoles = _roleManager.Roles.ToList();
+                // получаем список ролей, которые были добавлены
+                var addedRoles = roles.Except(userRoles);
+                // получаем роли, которые были удалены
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
         }
     }
 }
---
+++ EducateApp/Models/RoleInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace EducateApp.Models
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "3gt1@mail.ru";
+            string password = "_Aa123456";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("registeredUser") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("registeredUser"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User
+                {
+                    Email = adminEmail,
+                    UserName = adminEmail,
+                    LastName = "Куканов",
+                    Name = "Евгений",
+                    Patronymic = "Николаевич",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
